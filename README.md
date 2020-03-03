0d11cee581675cb095a2b2105e87f6fa4ea7391c
Gabriele Farei <JayFarei@users.noreply.github.com>
2020-03-03 19:37:45 +0000

    capturing input, randomising and moving pipes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Animating images using Canvas
+
+https://www.youtube.com/watch?v=L07i4g-zhDA&t=550s
--- flappyBird.js
+++ flappyBird.js
@@ -13,13 +13,11 @@ canvas.height= "512";
 
 // LOADING IMAGES
 // loading images and assigning them to the Image object
-
 var bird = new Image();
 var bg = new Image(); //background
 var fg = new Image(); //foreground
 var pipeNorth = new Image();
 var pipeSouth = new Image();
-
 
 bird.src = "images/bird.png";
 bg.src = "images/bg.png";
@@ -29,41 +27,89 @@ pipeSouth.src = "images/pipeSouth.png";
 
 
 
+
 // DEFINING VARIABLES & CONSTANTS
 
+var gap = 80; // gap between pipes
+var constant = 242 + gap; // defining Y coordinate by adding a gap from north pipe Y
 
-// Webkit browsers set the height and width property after the image is loaded.
-pipeSouth.onload = function() {
-    var pipeSouth_height = pipeSouth.height;
-    console.log(pipeSouth_height);
-    return pipeSouth_height
+// had to replace pipeNorth.height due to cache loading issues
+
+// Starting position & bird gravity
+var birdX = 10;
+var birdY = 150;
+
+var birdGravity = 1.5;
+
+
+// CAPTURE USER INPUT
+
+// Action
+function moveUp () {
+    birdY -= 20; // I decrease Y (move up) on every key press
+}
+
+// Input
+document.addEventListener("keydown", moveUp );
+
+
+
+// ANIMATION OF THE PIPES
+
+var pipe = []
+
+pipe [0] = {
+    x: canvas.width, y: 0 // I start on top and outside of the frame
 }
 
 
-
-var gap = 75;
-var constant = pipeSouth.height + gap;
-
-console.log(constant);
-
-// var constant = pipeSouth.height + gap
 
 
 
 // DRAW IMAGES
 
 function draw() {
-    requestAnimationFrame(draw); // note this has to be in first to draw the image
 
     ctx.drawImage(bg,0,0); 
-  
-    ctx.drawImage(pipeNorth,100,0);
 
-    ctx.drawImage(pipeSouth,100,0+constant)
-
-
+    for(var i = 0; i < pipe.length; i++) {
+        if (pipe[i].x == 125) { // whenever a pipe reaches 125, I push a new pipe in the array
+            pipe.push({
+                x: canvas.width, y: Math.floor(Math.random() * pipeNorth.height)-pipeNorth.height
+                // getting the largest integer of a random subset of pipeNorth.height - then subtracting to pipeNorth.height to get a negative Y number & pulling everything up.
     
+            })
+        }
+
+        pipe[i].x--; 
+
+        // x: pipes start outside of the canvas and move in frame by frame with x--
+
+        ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
+        ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y + constant); // gap + pipeNorth
+    
+    }
+   
+    ctx.drawImage(fg,0,canvas.height-fg.height); // to align it at the bottom
+
+    ctx.drawImage(bird,birdX,birdY)
+
+    birdY += birdGravity;
+
+    requestAnimationFrame(draw); // this will animate draw on every frame
+    
+
 }
 
-
+// Running the animation
 draw();
+
+// Action triggered by user input
+moveUp();
+
+
+
+
+// ctx.drawImage(pipeNorth,100, 1);
+
+// ctx.drawImage(pipeSouth,125, 0 + constant); 
